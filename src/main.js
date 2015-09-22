fab29a8783daeb64daf4c77ff77f2b8efac6b381
Milo <milonice@hotmail.com>
2015-09-23 00:05:21 +0200

    Passing data! React seems very smooth, happy with it so far.

--- src/main.js
+++ src/main.js
@@ -18,6 +18,32 @@ React.render(
   document.getElementById('headercontainer')
 );
 
+// article creation
+var Comment = React.createClass({
+  render: function() {
+    return (
+      <div className="comment">
+        <p className="commentAuthor">
+          {this.props.author}
+        </p>
+        {this.props.children}
+      </div>
+    );
+  }
+});
+
+// article render
+var CommentList = React.createClass({
+  render: function() {
+    return (
+      <div className="commentList">
+        <Comment author="Milo Roots">This is an article.</Comment>
+        <Comment author="Patrik Roots">This is yet another article.</Comment>
+      </div>
+    );
+  }
+});
+
 // body creation
 var MainBox = React.createClass({
   render: function() {
@@ -26,6 +52,9 @@ var MainBox = React.createClass({
         <p>
           This is the main container!
         </p>
+        <hr />
+        <p>Comments:</p>
+        <CommentList />
       </div>
     );
   }
